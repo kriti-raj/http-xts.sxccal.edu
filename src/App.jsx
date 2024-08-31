2d7d9f8ad88ad0332ee472394781cdf384b1dd82
Kriti Raj <6517kritiraj@gmail.com>
2024-08-31 05:56:34 +0530

    [update]: route fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import AboutUs from "./components/AboutUsPage.jsx";
 import Gallery from "./components/GalleryPage.jsx";
 import Events from "./components/EventsPage.jsx";
@@ -10,17 +10,15 @@ import Hero from "./components/Hero.jsx";
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Hero />} />
-          <Route path="/about-us" element={<AboutUs />} />
-          <Route path="/gallery" element={<Gallery />} />
-          <Route path="/events" element={<Events />} />
-          <Route path="/registrations" element={<Registration />} />
-          <Route path="/sponsor-us" element={<SponsorUs />} />
-          <Route path="/contact-us" element={<ContactUs />} />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/registrations" element={<Registration />} />
+        <Route path="/sponsor-us" element={<SponsorUs />} />
+        <Route path="/contact-us" element={<ContactUs />} />
+      </Routes>
     </>
   );
 }
