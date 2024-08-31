3b5960f76e80c8b56efca4b6222c1586ba3de7a4
Kriti Raj <6517kritiraj@gmail.com>
2024-08-31 06:07:04 +0530

    [update]: route fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
-// import { StrictMode } from "react";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    {/* <BrowserRouter> */}
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
+    {/* </BrowserRouter> */}
   </React.StrictMode>
 );
